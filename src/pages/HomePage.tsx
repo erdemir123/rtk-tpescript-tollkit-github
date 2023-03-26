fa27d428df14174203d1fed2d7d527c3c4f9fd33
erdemir123 <sadikerdemir93@gmail.com>
2023-03-26 13:53:36 +0300

    data list created

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -5,22 +5,19 @@ import { useSearchUsersQuery } from "../store/github/github.api";
 
 const HomePage = () => {
   const [search, setSearch] = useState<string>("");
-  const waiting = useWaiting(search)
-  const { isError, isLoading, data } = useSearchUsersQuery(
-    `${waiting}`,{skip:waiting.length <3}
-  );
+  const [dropDown, setdropDown] = useState<boolean>(false);
+  const waiting = useWaiting(search);
+  const { isError, isLoading, isSuccess, data } = useSearchUsersQuery(waiting, {
+    skip: waiting.length < 3,
+  });
+  console.log(data);
   const [user, setUser] = useState<ıUser[] | []>([]);
-  
-  useEffect(()=>{
-    console.log(waiting)
-    console.log(data?.items);
-  },[waiting])
   return (
     <div className="flex justify-center items-center pt-10 mx-auto h-screen w-screen">
       {isError && (
         <p className="text-center text-red-500"> SomeThing went Wrong...</p>
       )}
-      <div className="relative, w-[560px]">
+      <div className="relative w-[560px]">
         <input
           type="text"
           className="border py-2 px-4 w-full h-[42px] mb-2"
@@ -28,7 +25,18 @@ const HomePage = () => {
           value={search}
           onChange={(e) => setSearch(e.target.value)}
         />
-        <div className="absolute top-[42px] left-0 right-0 max-h-[200px] shadow-lg bg-white"></div>
+
+        <ul className="list-none absolute top-[42px] left-0 right-0 max-h-[200px] shadow-lg bg-white  overflow-y-scroll">
+          {isLoading && <p className="text-center">Loading...</p>}
+          {data?.items.map((user) => (
+            <li
+              key={user.id}
+              className="py-2 pl-4 hover:bg-gray-500 hover:text-white duration-300 transition-color cursor-pointer "
+            >
+              {user.login}
+            </li>
+          ))}
+        </ul>
       </div>
     </div>
   );
